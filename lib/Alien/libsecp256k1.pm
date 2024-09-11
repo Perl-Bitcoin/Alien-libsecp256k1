9c4a3655fab444dd3cca3a15434dc15992c1382b
bbrtj <bbrtj.pro@gmail.com>
2024-09-11 21:47:39 +0200

    Initial

---
+++ lib/Alien/libsecp256k1.pm
@@ -0,0 +1,39 @@
+package Alien::libsecp256k1;
+
+use v5.14;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::libsecp256k1 - Interface to libsecp256k1
+
+=head1 SYNOPSIS
+
+See L<Alien::Build::Manual::AlienUser>.
+
+=head1 DESCRIPTION
+
+This module should be used by another module that need
+L<libsecp256k1|https://github.com/bitcoin-core/secp256k1>.
+
+=head1 SEE ALSO
+
+L<Alien::Build>
+
+=head1 AUTHOR
+
+Bartosz Jarzyna E<lt>bbrtj.pro@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2024 by Bartosz Jarzyna
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
